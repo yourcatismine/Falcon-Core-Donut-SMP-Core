cdedbcaca4b786d6166ed720460f021626fdccd1
hwph <kiaraiscuteee@gmail.com>
2026-03-13 02:52:33 +0800

    Changes in Shards to Cards & Fixed Hext Color

--- src/main/java/com/h2ph/afk/AFKManager.java
+++ src/main/java/com/h2ph/afk/AFKManager.java
@@ -51,11 +51,11 @@ public class AFKManager {
 
         config = YamlConfiguration.loadConfiguration(configFile);
 
-        title = ChatColor.translateAlternateColorCodes('&', config.getString("title", "&#A9833Dᴀꜰᴋ"));
+        title = ChatColor.translateAlternateColorCodes('&', config.getString("title", "&6ᴀꜰᴋ"));
         subtitle = ChatColor.translateAlternateColorCodes('&',
                 config.getString("subtitle", "&fEarn one shard per minute"));
         actionbar = ChatColor.translateAlternateColorCodes('&',
-                config.getString("actionbar", "&7Next shard in &#A9833D<COUNTDOWN>s"));
+                config.getString("actionbar", "&7Next card in &6<COUNTDOWN>s"));
         countdownSeconds = config.getInt("countdown", 60);
         rewardAmount = config.getInt("reward-amount", 1);
 
@@ -76,9 +76,9 @@ public class AFKManager {
     private void saveDefaultConfig() {
         try {
             YamlConfiguration c = new YamlConfiguration();
-            c.set("title", "&#A9833Dᴀꜰᴋ");
+            c.set("title", "&6ᴀꜰᴋ");
             c.set("subtitle", "&fEarn one shard per minute");
-            c.set("actionbar", "&7Next shard in &#A9833D<COUNTDOWN>");
+            c.set("actionbar", "&7Next card in &6<COUNTDOWN>");
             c.set("countdown", 60);
             c.set("reward-amount", 1);
             c.set("entry-sound.sound", "minecraft:block.amethyst_block.chime");
